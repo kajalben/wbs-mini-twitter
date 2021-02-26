bed09b363366bf98d37a3e6b5aac5bfd5302fee4
Tim Schneider <timjo1@MacBook-Air.fritz.box>
2021-02-26 12:47:03 +0100

    add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
+import Profile from './components/Profile';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Main />
       <Footer />
+
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,18 @@
 
 const Footer = () =>{
     return(
-        <h1>Footer</h1>
+        
+        <div className="text-center p-3" style={{backgroundColor: '#bdbdbd', color: '#00227b'}}>
+
+        © 2021 Copyright: Avril, Diego, Janis, Kajal, Montaser, Priyanka, Tim
+        <br></br>
+        <a className="text-dark" href="https://github.com/kajalben/wbs-mini-twitter">
+            <b style={{color: '#00227b'}}>
+            mini-twitter
+            </b>
+             </a>
+
+      </div>
     );
 }
 
