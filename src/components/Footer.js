b5ebc63402abf3e18f3a79e7e46c44d1b29b9c8c
Kajalben <kajalsavani191@gmail.com>
2021-02-28 22:34:07 +0100

    fixed Footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,19 @@
-
-
-const Footer = () =>{
-    return(
-        
-        <div className="text-center p-3" style={{backgroundColor: '#bdbdbd', color: '#00227b'}}>
-
-        © 2021 Copyright: Avril, Diego, Janis, Kajal, Montaser, Priyanka, Tim
-        <br></br>
-        <a className="text-dark" href="https://github.com/kajalben/wbs-mini-twitter">
-            <b style={{color: '#00227b'}}>
-            mini-twitter
-            </b>
-             </a>
-
-      </div>
-    );
-}
+const Footer = () => {
+  return (
+    <div
+      className="footer-container"
+      style={{ backgroundColor: "#bdbdbd", color: "#00227b" }}
+    >
+      © 2021 Copyright: Avril, Diego, Janis, Kajal, Montaser, Priyanka, Tim
+      <br></br>
+      <a
+        className="text-dark"
+        href="https://github.com/kajalben/wbs-mini-twitter"
+      >
+        <b style={{ color: "#00227b" }}>mini-twitter</b>
+      </a>
+    </div>
+  );
+};
 
 export default Footer;
